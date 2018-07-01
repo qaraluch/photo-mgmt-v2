a669db2e364427f902174ddd0fc2a3a9d9018e34
qaraluch <prezenciq@gmail.com>
2018-07-01 18:14:30 +0200

    Add zip file name computing

--- src/task-backup.js
+++ src/task-backup.js
@@ -8,6 +8,11 @@ async function runTaskBackup(args) {
     await makeDir(cuBackup);
     listReadFiles(walkOutput);
     const backupPaths = copyPathsForBackup(walkOutput);
+    const backupFile = constructBackupFileName();
+    console.log("About to create zip file:");
+    console.log(` ${backupFile}`);
+    console.log("in:");
+    console.log(` ${cuBackup}`);
   } catch (error) {
     console.error(error);
   }
@@ -33,6 +38,22 @@ function listFiles(walkOutput) {
 function copyPathsForBackup(walkOutput) {
   const copyPaths = [...walkOutput.map(item => item.path)];
   return copyPaths;
+}
+
+function constructBackupFileName() {
+  const fileName = `cu-temp-arch-${getTimeStamp()}.zip`;
+  return fileName;
+}
+
+function getTimeStamp() {
+  return (
+    new Date()
+      .toISOString()
+      // modifies format of new Date '2012-11-04T14:51:06.157Z'
+      .replace(/T/, "_")
+      .replace(/:/g, "")
+      .replace(/\..+/, "")
+  );
 }
 
 module.exports = runTaskBackup;
