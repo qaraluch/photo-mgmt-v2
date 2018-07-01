c51e6364224e06566e27decad38d54b4b9adafd1
qaraluch <prezenciq@gmail.com>
2018-07-01 12:56:27 +0200

    Refactor code

--- src/task-backup.js
+++ src/task-backup.js
@@ -1,17 +1,32 @@
 const walk = require("qm-walk");
 
 async function runTaskBackup(args) {
-  const { cu: cuDir } = args;
-  const cuPaths = await getAllCuFiles(cuDir);
-  cuPaths.forEach(item => {
-    console.log("-->", item.name);
-  });
+  const { cu } = args;
+  const walkOutput = await getAllCuFiles(cu);
+  listReadFiles(walkOutput);
+  const backupPaths = copyPathsForBackup(walkOutput);
 }
 
 async function getAllCuFiles(scanPath) {
   const walkOutputExt = await walk({ path: scanPath });
   const paths = walkOutputExt.getExtendedInfo().result;
   return paths;
+}
+
+function listReadFiles(walkOutput) {
+  console.log("Read files:");
+  listFiles(walkOutput);
+}
+
+function listFiles(walkOutput) {
+  walkOutput.forEach(item => {
+    console.log("-->", item.name);
+  });
+}
+
+function copyPathsForBackup(walkOutput) {
+  const copyPaths = [...walkOutput.map(item => item.path)];
+  return copyPaths;
 }
 
 module.exports = runTaskBackup;
