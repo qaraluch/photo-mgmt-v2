d373ac62d4f9c9635cfbb71d4d59750b12b230fb
qaraluch <prezenciq@gmail.com>
2018-07-03 13:39:26 +0200

    Add long jpeg extention rename

--- src/rename.js
+++ src/rename.js
@@ -6,17 +6,32 @@ function pullInfoFromWalk(walkOutput) {
   return copyInfo;
 }
 
+const getExt = fileName => path.parse(fileName).ext;
+const getName = fileName => path.parse(fileName).name;
+
 function transformExtToLowerCase(item) {
   const oldName = item[1];
-  const nameExt = path.parse(oldName).ext.toLowerCase();
-  const nameBase = path.parse(oldName).name;
+  const nameExt = getExt(oldName).toLowerCase();
+  const nameBase = getName(oldName);
+  const newItem = [...item, `${nameBase}${nameExt}`];
+  return newItem;
+}
+
+function transformExtLongJpeg(item) {
+  const oldName = item.pop();
+  const oldExt = getExt(oldName);
+  const nameExt = oldExt === ".jpeg" ? ".jpg" : oldExt;
+  const nameBase = getName(oldName);
   const newItem = [...item, `${nameBase}${nameExt}`];
   return newItem;
 }
 
 function doRenameFiles(walkOutput) {
   const copyInfoToRename = pullInfoFromWalk(walkOutput);
-  const xform = R.compose(R.map(transformExtToLowerCase));
+  const xform = R.compose(
+    R.map(transformExtToLowerCase),
+    R.map(transformExtLongJpeg)
+  );
   const processedTransducer = R.into([], xform);
   const result = processedTransducer(copyInfoToRename);
   return result;
