c48a13c30a86ab818b9219bdebc9d2136b06f1b1
qaraluch <prezenciq@gmail.com>
2018-07-03 11:43:04 +0200

    Add transducer for file rename

--- src/task-presort-vids.js
+++ src/task-presort-vids.js
@@ -1,17 +1,19 @@
 const path = require("path");
 const makeDir = require("make-dir");
+const R = require("ramda");
 
 const { getTimeStamp } = require("./utils.js");
 const { getAllFiles } = require("./walker.js");
 
 async function runTaskPresortVids(args) {
   try {
-    const { cu, cuBackup } = args;
+    const { cu, cuSort } = args;
     const walkOutput = await getAllFiles(cu);
-    await makeDir(cuBackup);
+    await makeDir(cuSort);
     listReadFiles(walkOutput);
-    // const toBackupFilesPaths = copyPathsForBackup(walkOutput);
-    console.log("About to ...");
+    console.log("About to rename files...");
+    const renamedFiles = doRenameFiles(walkOutput);
+    renamedFiles.forEach(item => console.log(item[1], " --> ", item[2]));
     return;
   } catch (error) {
     console.error(error);
@@ -29,14 +31,23 @@ function listFiles(walkOutput) {
   });
 }
 
-// function copyPathsForBackup(walkOutput) {
-//   const copyPaths = [...walkOutput.map(item => item.path)];
-//   return copyPaths;
-// }
+//TODO: move to: rename.js
+function pullInfoFromWalk(walkOutput) {
+  const copyInfo = [...walkOutput.map(item => [item.path, item.name])];
+  return copyInfo;
+}
 
-// function constructBackupFileName() {
-//   const fileName = `cu-temp-arch-${getTimeStamp()}.zip`;
-//   return fileName;
-// }
+function transformExtToLowerCase(item) {
+  const newItem = [...item, "..."];
+  return newItem;
+}
+
+function doRenameFiles(walkOutput) {
+  const copyInfoToRename = pullInfoFromWalk(walkOutput);
+  const xform = R.compose(R.map(transformExtToLowerCase));
+  const processedTransducer = R.into([], xform);
+  const result = processedTransducer(copyInfoToRename);
+  return result;
+}
 
 module.exports = runTaskPresortVids;
