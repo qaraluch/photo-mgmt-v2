bdfd7bf84b05380179b0be9b2ae3421e0975ab71
qaraluch <prezenciq@gmail.com>
2018-07-04 17:39:13 +0200

    Move get exif data fn to separate file

---
+++ src/exif.js
@@ -0,0 +1,27 @@
+const exiftool = require("node-exiftool");
+const exiftoolBin = require("dist-exiftool");
+//[Sobesednik/node-exiftool: A Node.js interface to exiftool command-line application.](https://github.com/Sobesednik/node-exiftool)
+
+const throat = require("throat");
+//[ForbesLindesay/throat: Throttle a collection of promise returning functions](https://github.com/ForbesLindesay/throat)
+
+async function getExifData(walkOutput) {
+  const ep = new exiftool.ExiftoolProcess(exiftoolBin);
+  await ep.open();
+  async function runCheck(item) {
+    try {
+      item.exif = await ep.readMetadata(item.path, ["-File:all"]);
+      return item;
+    } catch (error) {
+      throw error;
+    }
+  }
+  const promises = walkOutput.map(throat(8, runCheck, Promise.resolve()));
+  const newWalkOutput = await Promise.all(promises);
+  await ep.close();
+  return newWalkOutput;
+}
+
+module.exports = {
+  getExifData
+};
