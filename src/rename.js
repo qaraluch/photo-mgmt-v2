632dfb0a2598ee8e9022889e14e9a5d10d7f82f6
qaraluch <prezenciq@gmail.com>
2018-07-04 18:36:48 +0200

    Refactor rename code

--- src/rename.js
+++ src/rename.js
@@ -6,6 +6,21 @@ const { getExifData } = require("./exif.js");
 const getExt = fileName => path.parse(fileName).ext;
 const getName = fileName => path.parse(fileName).name;
 
+async function doRenameFiles(walkOutput) {
+  const infoToRename = pullInfoFromWalk(walkOutput);
+  const infoFromFileName = R.map(readPreexistedData, infoToRename);
+  const infoWithExif = await getExifData(infoFromFileName);
+  const xform = R.compose(
+    R.map(transformExtToLowerCase),
+    R.map(transformExtLongJpeg),
+    R.map(addVersions),
+    R.map(reassemblyFileName)
+  );
+  const transducer = R.into([], xform);
+  const renamedFiles = transducer(infoWithExif);
+  return renamedFiles;
+}
+
 function pullInfoFromWalk(walkOutput) {
   const copyInfo = [
     ...walkOutput.map(item => ({
@@ -24,22 +39,17 @@ function pullInfoFromWalk(walkOutput) {
   return copyInfo;
 }
 
-async function doRenameFiles(walkOutput) {
-  const copyInfoToRename = pullInfoFromWalk(walkOutput);
-  const xform = R.compose(
-    R.map(transformExtToLowerCase),
-    R.map(transformExtLongJpeg),
-    R.map(readPreexistedData),
-    R.map(addVersions)
-  );
-  const processedTransducer = R.into([], xform);
-  const renamedFiles = processedTransducer(copyInfoToRename);
-  const withExifData = await getExifData(renamedFiles);
-  withExifData.forEach(item => {
-    console.log(item.oldName, " --> ", item.exif.Model);
-  });
-  const result = R.map(reassemblyFileName, renamedFiles);
-  return result;
+//ES9 / node.js ^10.3.0
+const regexDate = /(?<date>\d{4}-\d{2}-\d{2}\s\d{2}\.\d{2}\.\d{2})(-)?(?<version>\d)?(\s)?([-|—])?(\s)?(?<comment>.+)?/;
+
+function readPreexistedData(item) {
+  const { oldBaseName } = item;
+  const match = regexDate.exec(oldBaseName);
+  item.date = match && match.groups.date;
+  item.version = match && match.groups.version;
+  item.comment = match && match.groups.comment;
+  item.comment && (item.comment = item.comment.trim());
+  return item;
 }
 
 function transformExtToLowerCase(item) {
@@ -51,19 +61,6 @@ function transformExtLongJpeg(item) {
   const oldExt = item.newExt;
   const newExt = oldExt === ".jpeg" ? ".jpg" : oldExt;
   item.newExt = newExt;
-  return item;
-}
-
-//ES9 / node.js ^10.3.0
-const regexDate = /(?<date>\d{4}-\d{2}-\d{2}\s\d{2}\.\d{2}\.\d{2})(-)?(?<version>\d)?(\s)?([-|—])?(\s)?(?<comment>.+)?/;
-
-function readPreexistedData(item) {
-  const { oldBaseName } = item;
-  const match = regexDate.exec(oldBaseName);
-  item.date = match && match.groups.date;
-  item.version = match && match.groups.version;
-  item.comment = match && match.groups.comment;
-  item.comment && (item.comment = item.comment.trim());
   return item;
 }
 
@@ -81,6 +78,7 @@ function reassemblyFileName(item) {
   } else {
     item.newName = oldName;
   }
+  // console.log("item ", item.newName);
   return item;
 }
 
