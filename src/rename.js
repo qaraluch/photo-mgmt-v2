d07e2eba9502cc300d43ecd488f65df59932a14d
qaraluch <prezenciq@gmail.com>
2018-07-09 13:12:47 +0200

    Move parseExistedFileName fn to utils

--- src/rename.js
+++ src/rename.js
@@ -2,13 +2,14 @@ const path = require("path");
 const R = require("ramda");
 
 const { getExifData } = require("./exif.js");
+const { parseExistedFileName } = require("./utils.js");
 
 const getExt = fileName => path.parse(fileName).ext;
 const getName = fileName => path.parse(fileName).name;
 
 async function doRenameFiles(walkOutput) {
   const infoToRename = pullInfoFromWalk(walkOutput);
-  const infoFromFileName = R.map(readPreexistedData, infoToRename);
+  const infoFromFileName = R.map(parseExistedFileName, infoToRename);
   const infoWithExif = await getExifData(infoFromFileName);
   const xform = R.compose(
     R.map(transformExtToLowerCase),
@@ -25,6 +26,7 @@ function pullInfoFromWalk(walkOutput) {
   const copyInfo = [
     ...walkOutput.map(item => ({
       path: item.path,
+      stats: item.stats,
       oldName: item.name,
       oldBaseName: getName(item.name),
       newExt: getExt(item.name),
@@ -37,19 +39,6 @@ function pullInfoFromWalk(walkOutput) {
     }))
   ];
   return copyInfo;
-}
-
-//ES9 / node.js ^10.3.0
-const regexDate = /(?<date>\d{4}-\d{2}-\d{2}\s\d{2}\.\d{2}\.\d{2})(-)?(?<version>\d)?(\s)?([-|—])?(\s)?(?<comment>.+)?/;
-
-function readPreexistedData(item) {
-  const { oldBaseName } = item;
-  const match = regexDate.exec(oldBaseName);
-  item.date = match && match.groups.date;
-  item.version = match && match.groups.version;
-  item.comment = match && match.groups.comment;
-  item.comment && (item.comment = item.comment.trim());
-  return item;
 }
 
 function transformExtToLowerCase(item) {
--- src/utils.js
+++ src/utils.js
@@ -9,6 +9,21 @@ function getTimeStamp() {
   );
 }
 
+//ES9 / node.js ^10.3.0
+const regexFileName = /(?<date>\d{4}-\d{2}-\d{2}\s\d{2}\.\d{2}\.\d{2})(-)?(?<version>\d)?(\s)?([-|—])?(\s)?(?<comment>.+)?/;
+
+function parseExistedFileName(item) {
+  const { oldBaseName } = item;
+  const match = regexFileName.exec(oldBaseName);
+  item.date = match && match.groups.date;
+  item.version = match && match.groups.version;
+  item.comment = match && match.groups.comment;
+  item.comment && (item.comment = item.comment.trim());
+  return item;
+}
+
 module.exports = {
-  getTimeStamp
+  getTimeStamp,
+  regexFileName,
+  parseExistedFileName
 };
