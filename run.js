abb0f83ec6bad6525b5668b060098e9020c94d08
qaraluch <prezenciq@gmail.com>
2018-09-26 13:18:44 +0200

    Clean up run.js

--- run.js
+++ run.js
@@ -1,90 +1,12 @@
 const { runThis } = require("./src/index.js");
 
 //Manual run:
-//Edit and run this by: `nps run`
+//Edit and run this by: `nps`
+// See: config.js for available options reference.
 
-const commandBackup = {
-  command: "backup",
-  config: "configTest",
-  checkArchive: true
-};
-
-const commandBackupReal = {
-  command: "backup",
-  config: "configCU",
-  checkArchive: true
-};
-
-// runThis(commandBackup);
-// runThis(commandBackupReal);
-
-const commandPresort = {
+const args = {
   command: "presort",
-  config: "configTest",
-  dryRun: false
+  config: "dev"
 };
 
-const commandPresortReal = {
-  command: "presort",
-  config: "configCU",
-  dryRun: false
-};
-
-// runThis(commandPresort);
-// runThis(commandPresortReal);
-
-const commandRename = {
-  command: "rename",
-  config: "configTestRename",
-  dryRun: false,
-  tag: "myTag"
-};
-
-// runThis(commandRename);
-
-const commandRenameAfterParentDir = {
-  command: "rename",
-  config: "configTestRename",
-  dryRun: false,
-  renameAfterParentDir: true
-};
-
-const commandRenameAfterParentDirInputDir = {
-  command: "rename",
-  config: "configTestRename",
-  dryRun: false,
-  renameAfterParentDir: true,
-  inputDir: "./test/fixtures/cu-presort-rename/some-dir/"
-};
-
-const commandRenameAfterParentDirExcludeDirs = {
-  command: "rename",
-  config: "configTestRename",
-  dryRun: false,
-  renameAfterParentDir: true,
-  excludeDirs: "some-dir, terefere"
-};
-
-const commandRenameAfterParentDirReal = {
-  command: "rename",
-  config: "configCU",
-  dryRun: true,
-  renameAfterParentDir: true,
-  excludeDirs:
-    "_camera-save, _filmiki, _grafa_assets, _luzne, _modyf, _ogolne, _org, _piony, _rys_duplikaty, _slides-ep, _slides-nasze, _temp"
-};
-
-const commandRenameAfterParentDirRealInput = {
-  command: "rename",
-  config: "configCU",
-  dryRun: false,
-  renameAfterParentDir: true,
-  inputDir: "/mnt/g/Dropbox/mydrocsort/warszawa"
-};
-
-// runThis(commandRenameAfterParentDir);
-// runThis(commandRenameAfterParentDirExcludeDirs);
-// runThis(commandRenameAfterParentDirInputDir);
-
-// runThis(commandRenameAfterParentDirReal);
-// runThis(commandRenameAfterParentDirRealInput);
+runThis(args);
